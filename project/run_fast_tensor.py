ab1d100af32dbb99fc7eb6164cc16b5e3dec2b50
lizhieffe <lizhieffe@gmail.com>
2025-01-27 06:31:23 +0000

    colab update

--- project/run_fast_tensor.py
+++ project/run_fast_tensor.py
@@ -29,8 +29,13 @@ class Network(minitorch.Module):
         self.layer3 = Linear(hidden, 1, backend)
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        y = self.layer1(x)
+        y = y.relu()
+        y = self.layer2(y)
+        y = y.relu()
+        y = self.layer3(y)
+        y = y.sigmoid()
+        return y
 
 
 class Linear(minitorch.Module):
@@ -43,8 +48,7 @@ class Linear(minitorch.Module):
         self.out_size = out_size
 
     def forward(self, x):
-        # TODO: Implement for Task 3.5.
-        raise NotImplementedError("Need to implement for Task 3.5")
+        return x @ self.weights.value + self.bias.value
 
 
 class FastTrain:
